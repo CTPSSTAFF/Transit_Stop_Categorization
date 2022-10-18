b76bc01f3b2373f7e00d0394784e887aa7bceb2a
SophieFox26 <sfox@ctps.org>
2022-10-18 11:24:03 -0400

    More Service ID changes for the RTAs

--- Analysis/Running_Freq_Stops.R
+++ Analysis/Running_Freq_Stops.R
@@ -3,16 +3,51 @@ library(tidyverse)
 #install.packages("gtfstools")
 
 # TO DO: Might want to include more service IDs
+# trips <- GTFS_Output$trips
+# stop_times <- GTFS_Output$stop_times
+# cal <- GTFS_Output$calendar
+# cal_att <- GTFS_Output$calendar_attributes
+# 
+# stop_time_by_trip <- left_join(trips, stop_times, by = "trip_id")
+# 
+# cal_att_dates_fixed <- cal_att %>% 
+#   full_join(cal, by = "service_id")
+# cal_att_dates_fixed$start_date <- gsub("-", "", cal_att_dates_fixed$start_date) %>%
+#   as.double()
+# cal_att_dates_fixed$end_date <- gsub("-", "", cal_att_dates_fixed$end_date)%>%
+#   as.double()
+# 
+# 
+# cal_att_filt <- cal_att_dates_fixed %>%
+#   filter(start_date <= 20221008 & end_date >= 20221008) %>% 
+#   filter(wednesday == 1 | saturday == 1 | sunday == 1) %>%
+#   distinct()
+# 
+# stop_time_by_trip_filter <- stop_time_by_trip |> filter(service_id %in% CATA_WD_id) 
 
 ### BAT  ----------------------------------------------------------
 
 BAT_gtfs_path <- "../GTFS_data/brockton-ma-us.zip"
 BAT_detailedpath <- "../Output/frequent_stops_detail_BAT.csv"
 BAT_summarypath <- "../Output/frequent_stops_summary_BAT.csv"
+#GTFS_Output <- import_gtfs(BAT_gtfs_path)
 
 
 # used "Year Round Starting 3/1/21" for weekday, saturday, sunday
-BAT_WD_id <- "c_21163_b_29144_d_31" 
+# WD <- cal_att_filt %>%
+#   filter(wednesday == 1)
+# BAT_WD_id <- WD$service_id
+# 
+# SA <- cal_att_filt %>%
+#   filter(saturday == 1)
+# BAT_SA_id <- SA$service_id
+# 
+# SU <- cal_att_filt %>%
+#   filter(sunday == 1)
+# BAT_SU_id <- SU$service_id
+# Because the only ID with Sunday is every day, it is classified as WD
+
+BAT_WD_id <- c("c_21163_b_29144_d_31", "c_1747_b_78103_d_31") # no duplicates, service ids are for different routes
 BAT_SA_id <- "c_21163_b_29144_d_32"
 BAT_SU_id <- "c_21163_b_29144_d_64"
   
@@ -22,9 +57,25 @@ BAT_SU_id <- "c_21163_b_29144_d_64"
 CATA_gtfs_path <- "../GTFS_data/capeann-ma-us.zip"
 CATA_detailedpath <- "../Output/frequent_stops_detail_CATA.csv"
 CATA_summarypath <- "../Output/frequent_stops_summary_CATA.csv"
+#GTFS_Output <- import_gtfs(CATA_gtfs_path)
 
 # Year Round starting 2/21/22 weekday and saturday
-CATA_WD_id <- "c_24289_b_33923_d_31" 
+# WD <- cal_att_filt %>%
+#   filter(wednesday == 1)
+# CATA_WD_id <- WD$service_id
+# 
+# SA <- cal_att_filt %>%
+#   filter(saturday == 1)
+# CATA_SA_id <- SA$service_id
+# 
+# SU <- cal_att_filt %>%
+#   filter(sunday == 1)
+# CATA_SU_id <- SU$service_id
+
+
+
+CATA_WD_id <- c("c_24289_b_33923_d_31" , "c_21058_b_28942_d_31", "c_21045_b_78058_d_31")
+#  The WD service ids are all for different routes
 CATA_SA_id <- "c_24289_b_33923_d_32" 
 CATA_SU_id <- ""
 # No Sunday 
@@ -35,6 +86,7 @@ CATA_SU_id <- ""
 GATRA_gtfs_path <- "../GTFS_data/gatra-ma-us.zip"
 GATRA_detailedpath <- "../Output/frequent_stops_detail_GATRA.csv"
 GATRA_summarypath <- "../Output/frequent_stops_summary_GATRA.csv"
+#GTFS_Output <- import_gtfs(GATRA_gtfs_path)
 
 # Year Round starting 10/3/22
 # Weekday MWThF and Tuesday 
@@ -50,6 +102,7 @@ GATRA_SU_id <- ""
 LRTA_gtfs_path <- "../GTFS_data/lowell-ma-us.zip"
 LRTA_detailedpath <- "../Output/frequent_stops_detail_LRTA.csv"
 LRTA_summarypath <- "../Output/frequent_stops_summary_LRTA.csv"
+#GTFS_Output <- import_gtfs(LRTA_gtfs_path)
 
 # Year Round starting 8/15/22
 LRTA_WD_id <- "c_67333_b_77545_d_31" 
@@ -63,6 +116,7 @@ LRTA_SU_id <- ""
 MVRTA_gtfs_path <- "../GTFS_data/merrimackvalley-ma-us.zip"
 MVRTA_detailedpath <- "../Output/frequent_stops_detail_MVRTA.csv"
 MVRTA_summarypath <- "../Output/frequent_stops_summary_MVRTA.csv"
+#GTFS_Output <- import_gtfs(MVRTA_gtfs_path)
 
 # Using Year Round starting 9/6/22 
 MVRTA_WD_id <- "c_67487_b_77930_d_31" 
@@ -78,6 +132,7 @@ MVRTA_SU_id <- ""
 MW_gtfs_path <- "../GTFS_data/metrowest-ma-us.zip"
 MW_detailedpath <- "../Output/frequent_stops_detail_MW.csv"
 MW_summarypath <- "../Output/frequent_stops_summary_MW.csv"
+#GTFS_Output <- import_gtfs(MW_gtfs_path)
 
 # Chosen by using 9/30/22-10/01/22-10/02/22
 MW_WD_id <- "2576ab55-fdab-479b-a7e0-61e3113a76aa" 
@@ -113,7 +168,15 @@ summarized_output <- freq_service_summary(stop_headways) %>% left_join(GTFS_Outp
 write_csv(summarized_output, path = summ) 
 
 
+## Mapping the frequent stops##
+stops_geo <- summarized_output %>%
+  filter(frequent_stop == TRUE) %>%
+  #filter(passing_values >=5) %>%
+  st_as_sf(coords= c("stop_lon", "stop_lat")) %>% 
+  st_set_crs(4326) %>%
+  st_transform(26986)
 
+mapview(stops_geo, zcol = 'frequent_stop')
 
 ### Notes -------------------------------------------------------------------
 # Took the service IDs from calendar_attributes of each RTA
