add529d97cf8db8fe0287c5c9c4f3ed10d4a7955
SophieFox26 <sfox@ctps.org>
2023-04-05 15:25:41 -0400

    MBTA

--- Analysis/OPMI_MBTA.R
+++ Analysis/OPMI_MBTA.R
@@ -7,9 +7,9 @@ library(gtfstools)
 # Load in OPMI data: 
 
 freq_WD <- read_csv("GTFS_Data/MBTA/freqstops_fa22_wkdy15.csv") %>% 
-  mutate(frequent_stop = TRUE)
+  mutate(WD_frequent_stop = TRUE)
 freq_SA <- read_csv("GTFS_Data/MBTA/freqstops_fa22_sat20.csv")  %>% 
-  mutate(frequent_stop = TRUE)
+  mutate(SA_frequent_stop = TRUE)
 
 allStops_WD <- read_csv("GTFS_Data/MBTA/allstops_fa22_wkdy.csv")
 allStops_SA <- read_csv("GTFS_Data/MBTA/allstops_fa22_sat.csv")
@@ -17,4 +17,16 @@ allStops_SA <- read_csv("GTFS_Data/MBTA/allstops_fa22_sat.csv")
 
 
 WD_freq_summ <- full_join(allStops_WD, freq_WD) %>% 
-  mutate(frequent_stop = if_else(is.na(frequent_stop), FALSE, TRUE))
+  mutate(WD_frequent_stop = if_else(is.na(WD_frequent_stop), FALSE, TRUE))
+
+SA_freq_summ <- full_join(allStops_SA, freq_SA) %>% 
+  mutate(SA_frequent_stop = if_else(is.na(SA_frequent_stop), FALSE, TRUE))
+
+# needs to be frequent on both the weekday and saturday to be considered frequent
+MBTA_freq_summary <- full_join(WD_freq_summ,SA_freq_summ ) %>% 
+  mutate(MBTA_frequent_stop = case_when(WD_frequent_stop == TRUE & SA_frequent_stop == TRUE ~ TRUE, .default = FALSE) ) %>% 
+  st_as_sf(coords= c("stop_lon", "stop_lat")) %>% st_set_crs(4326) %>% st_transform(26986)
+  
+
+
+         
